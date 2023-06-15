05bb09fb0a9dc7a641f47742ac70e8a0cc1bc6fa
CreativeDeveloperAbhishek <133561786+CreativeDeveloperAbhishek@users.noreply.github.com>
2023-06-15 21:10:00 +0530

    Add files via upload

---
+++ javascriprfiles/script.js
@@ -0,0 +1,12 @@
+alert('Do not share any confidential data ')
+function saveMessage() {
+  // Get the user's message from the textarea
+  var message = document.getElementById("message").value;
+  
+  // Code to save the message goes here...
+  
+  // Redirect the user to the OpenAI website in a new page
+  window.open("https://openai.com", "_blank");
+}
+
+
