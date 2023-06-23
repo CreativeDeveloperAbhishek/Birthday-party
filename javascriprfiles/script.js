173edeb245d962bab63c8d2ff67465fdda0b0a73
CreativeDeveloperAbhishek <133561786+CreativeDeveloperAbhishek@users.noreply.github.com>
2023-06-23 10:57:26 +0530

    Update script.js

--- javascriprfiles/script.js
+++ javascriprfiles/script.js
@@ -1,12 +1,12 @@
 alert('Do not share any confidential data ')
 function saveMessage() {
   // Get the user's message from the textarea
-  var message = document.getElementById("message").value;
+  document.getElementById('message').innerHTML = 'Your message is submitted succesfully.';
   
   // Code to save the message goes here...
   
   // Redirect the user to the OpenAI website in a new page
-  window.open("https://openai.com", "_blank");
+ 
 }
 
 
